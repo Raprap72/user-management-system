cc8a0e8b1cb650dc5a09769f54b8608359960196
Clint Tuyor <nanisama98@gmail.com>
2025-05-14 15:47:50 +0800

    _services DONE

---
+++ src/app/_models/account.ts
@@ -0,0 +1,13 @@
+import { Role } from './role';
+
+export interface Account {
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: Role;
+    jwtToken?: string;
+    refreshToken?: string;
+    isVerified: boolean;
+}
