4621e43c3f293ba99ef48ee6c2ff646cc4798ada
unknown <YY243@ccs.local>
2025-05-19 03:13:19 -0700

    main

---
+++ backend/config/index.js
@@ -0,0 +1,17 @@
+// (Make sure require('dotenv').config(); is called in server.js first!)
+
+module.exports = {
+  database: {
+    host: "153.92.15.31",
+    port: 3306,
+    user: "u875409848_teopiz",
+    password: "9T2Z5$3UKkgSYzE",
+    database: "u875409848_teopiz",
+  },
+  secret: "terces",
+};
+
+// Then, in other files, use it like:
+// const config = require('./config'); // Adjust path as needed
+// const sequelize = new Sequelize(config.database.database, ...);
+// const jwtSecret = config.secret;
