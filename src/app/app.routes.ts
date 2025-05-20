f52f2c72daa23aabfba33b76b7efa73caaa7ff5c
Raprap72 <wisrtalon@gmail.com>
2025-05-20 15:44:56 +0800

    Alon Full Stack

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  // Define your routes here
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
-];
